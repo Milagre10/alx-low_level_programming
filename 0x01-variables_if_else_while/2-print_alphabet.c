92ce577a904b825b6416926d9f066cda3748c8b6
Milagre10 <milorov13@gmail.com>
2023-07-17 17:58:50 -0700

    milo

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,14 @@
  * Return :Always 0
  */
  int main(void)
- {
-	 int n = 97;
-	 while(n <= 122)
-	 {
-		 putchar(n);
-		 n++;
-	 }
-	 putchar('\n');
-	 return(0);
- }
+{
+    int n = 97;
+
+    while (n <= 122)
+    {
+        putchar(n);
+        n++;
+    }
+    putchar('\n');
+    return (0);
+}
