ec073e13eeda09d4636b390b5741cfabf93bc553
Milagre10 <milorov13@gmail.com>
2023-07-17 18:02:24 -0700

    milo

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,22 @@
-#include<stdio.h>
-/**
- * main- Entry point
- * Description : Return lowercase
- * Return :Always 0
- */
- int main(void)
-{
-    int n = 97;
+#include <stdio.h>
 
-    while (n <= 122)
-    {
-        putchar(n);
-        n++;
-    }
-    putchar('\n');
-    return (0);
+/**
+
+main - Entry point
+
+Description: Prints lowercase letters
+
+Return: Always 0
+*/
+int main(void)
+{
+int n = 97;
+
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
 }
